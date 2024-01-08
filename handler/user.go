02d7df73fc5145a1dbf97cf9f6146e83eed7243a
Ethea2 <nathandres2001@gmail.com>
2024-01-09 03:03:56 +0400

    added: querying

--- handler/user.go
+++ handler/user.go
@@ -3,9 +3,17 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/google/uuid"
+
+	db "github.com/Ethea2/popio-server/database"
 )
 
-type User struct{}
+type User struct {
+	ID       uuid.UUID `gorm:"primary_key" json:"id"`
+	Username string    `                   json:"username"`
+	Password string    `                   json:"password"`
+}
 
 func (u *User) SignUp(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("SignUp")
@@ -20,5 +28,7 @@ func (u *User) Logout(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u *User) GetUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Get user")
+	db.Database.First(&u)
+
+	fmt.Println(u.Username)
 }
