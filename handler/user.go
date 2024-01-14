4f04948ff29ee6a13ed31c5f11bcbb4bc3609a93
Ethea2 <nathandres2001@gmail.com>
2024-01-15 00:34:53 +0400

    added: signup functionality

--- handler/user.go
+++ handler/user.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 
 	db "github.com/Ethea2/popio-server/database"
+	"github.com/Ethea2/popio-server/models"
 )
 
 type User struct {
@@ -25,7 +28,39 @@ type UserGroup struct {
 }
 
 func (u *User) SignUp(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("SignUp")
+	w.Header().Set("Content-Type", "application/json")
+
+	type InputType struct {
+		Username string
+		Password string
+	}
+	var userinput InputType
+	err := json.NewDecoder(r.Body).Decode(&userinput)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userinput.Password), 12)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id := uuid.New()
+
+	u.Password = string(hashedPassword)
+	u.ID = id
+	u.Username = userinput.Username
+	db.Database.Create(&u)
+	res := models.Response{
+		StatusCode: 200,
+		Message:    "success",
+		Data:       u,
+	}
+	jsonResponse, err := json.Marshal(res)
+
+	w.Write(jsonResponse)
 }
 
 func (u *User) Login(w http.ResponseWriter, r *http.Request) {
@@ -38,7 +73,7 @@ func (u *User) Logout(w http.ResponseWriter, r *http.Request) {
 
 func (u *User) GetUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	db.Database.First(&u, "id = ?", "03098d03-a233-4811-b4c2-6edb3fb42075")
-	fmt.Print(u)
+	userId := chi.URLParam(r, "id")
+	db.Database.First(&u, "id = ?", userId)
 	json.NewEncoder(w).Encode(u)
 }
